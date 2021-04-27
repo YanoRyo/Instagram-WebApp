a49bd1cd3ceb63d13b74f9b95349a4d3ad7089bf
YanoRyo <yanoryo7032@gmail.com>
2021-04-27 10:56:16 +0900

    プロフィールページの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ==========profile_photoカラムに何もない場合、デフォルトのアイコンを表示==========
+  def avatar_url(user)
+    return user.profile_photo unless user.profile_photo.nil?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://i.pinimg.com/564x/66/a1/01/66a101499104c54d4b57dd4db065393a.jpg"
+
+  end
+  # ==========profile_photoカラムに何もない場合、デフォルトのアイコンを表示==========
+
 end
