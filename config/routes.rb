446c175eec806098fa8e369f00d13e92e0f2600f
YanoRyo <yanoryo7032@gmail.com>
2021-04-29 16:27:15 +0900

    投稿機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, # ここの行にカンマを追加
     controllers: { registrations: 'registrations' } # ここの行を追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'posts#index'
   # ここにusersコントローラーのshowアクションのルーティングを追加する
   get 'users/:id', to:'users#show',as:'user'
+
+  #ポスト機能のルート
+  resources :posts, only: %i(new create index) do
+    resources :phots, only: %i(create)
+  end
+  #ポスト機能のルート
 
 end
